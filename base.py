494b482b29137e4906c095dd8b24e1e5f8954950
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-10-26 21:04:42 -0400

    Add JSON

--- base.py
+++ base.py
@@ -1,18 +1,23 @@
 import discord
+import json
 from secrets.token_secret import get_token
 
+
 TOKEN = get_token()
-
 client = discord.Client()
-
-
+data = open('replies.json')
+replies = json.load(data)
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('Yeet'):
-        msg = 'Yeet {0.author.mention}'.format(message)
-        await respond(message.channel, msg)
+    elif message.content.lower() in replies.keys():
+        await respond(message.channel, replies[message.content] if not message.content.endswith('.') else replies[message.content] + '.') 
+    #if message.content.startswith('Yeet'):
+    #    msg = 'Yeet {0.author.mention}'.format(message)
+    #    await respond(message.channel, msg)
+    if message.author == 'Greg':
+        print('Greg Meme')
 
 @client.event
 async def respond(channel, msg):
