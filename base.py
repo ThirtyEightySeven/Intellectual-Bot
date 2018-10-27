d8785cce2ce5c9cc1c2156aa37ff84ec13a12142
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-10-26 21:59:56 -0400

    Add upvote and downvote feature

--- base.py
+++ base.py
@@ -11,19 +11,35 @@ replies = json.load(data)
 
 @client.event
 async def on_message(message):
+    print(type(message.channel))
+
     if message.author == client.user:
         return
     elif message.content.lower().endswith('.'):
         await respond(message.channel, replies[message.content.lower()[:-1]] + '.')
     elif message.content.lower() in replies.keys():
         await respond(message.channel, replies[message.content.lower()])
-    elif message.author.id == '148254683704721408' and message.channels == '505550463408799774':
-        await respond(message.channel, 'Greg Meme™')
+    elif message.channel.id == '505553720294113281':
+        await react('hello', None)
+    elif message.channel.id == '505550463408799774':
+            if message.author.id == '148254683704721408':
+                print('DEBUG: GREG')          
+              #  await respond(message.channel, str(message.channel))
+                await respond(message.channel, 'Greg Meme™')
 
 
 @client.event
 async def respond(channel, msg):
     await client.send_message(channel, msg)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.channel.id == '505553720294113281':
+        await client.add_reaction(message, ':upvote:505555867031306260')
+        await client.add_reaction(message, ':downvote:505555876892114944')
 
 
 @client.event
